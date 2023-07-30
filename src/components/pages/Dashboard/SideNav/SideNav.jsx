46d8e14ee1941fb2eb6f9be11b40ce8dfee70e05
Jami <jamsheduddin03@gmail.com>
2023-07-30 20:35:50 +0600

    dashboad navigation fixes

--- src/components/pages/Dashboard/SideNav/SideNav.jsx
+++ src/components/pages/Dashboard/SideNav/SideNav.jsx
@@ -1,11 +1,9 @@
 import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./SideNav.css";
+import SideNavItems from "../SideNavItems";
 
 const SideNav = ({ displayName }) => {
-  const [admin, setAdmin] = useState(false);
-  const [student, setStudent] = useState(false);
-  const [instructor, setInstructor] = useState(true);
   return (
     <div className="bg-white shadow-lg h-screen lg:w-[20vw] w-72 pt-6">
       <h1 className="pl-5 text-2xl font-semibold py-3">{displayName}</h1>
@@ -21,88 +19,21 @@ const SideNav = ({ displayName }) => {
           </NavLink>
         </li>
 
-        {student && (
-          <>
-            <li>
-              <NavLink
-                to={"/dashboard/selected-classes"}
-                className={({ isActive }) =>
-                  isActive ? "bg-gradient-to-r from-slate-300" : ""
-                }
-              >
-                Selected classes
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={"/dashboard/enrolled-classes"}
-                className={({ isActive }) =>
-                  isActive ? "bg-gradient-to-r from-slate-300" : ""
-                }
-              >
-                enrolled classes
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={"/dashboard/payment-history"}
-                className={({ isActive }) =>
-                  isActive ? "bg-gradient-to-r from-slate-300" : ""
-                }
-              >
-                payment history
-              </NavLink>
-            </li>
-          </>
-        )}
-        {instructor && (
-          <>
-            <li>
-              <NavLink
-                to={"/dashboard/add-class"}
-                className={({ isActive }) =>
-                  isActive ? "bg-gradient-to-r from-slate-300" : ""
-                }
-              >
-                Add class
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={"/dashboard/my-classes"}
-                className={({ isActive }) =>
-                  isActive ? "bg-gradient-to-r from-slate-300" : ""
-                }
-              >
-                My classes
-              </NavLink>
-            </li>
-          </>
-        )}
-        {admin && (
-          <>
-            <li>
-              <NavLink
-                to={`/dashboard/${"manage-user"}`}
-                className={({ isActive }) =>
-                  isActive ? "bg-gradient-to-r from-slate-300" : ""
-                }
-              >
-                Manage users
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={"/dashboard/manage-classes"}
-                className={({ isActive }) =>
-                  isActive ? "bg-gradient-to-r from-slate-300" : ""
-                }
-              >
-                Manage classes
-              </NavLink>
-            </li>
-          </>
-        )}
+        {/* user specific dashboard navigation */}
+        <SideNavItems />
+      </ul>
+
+      {/* back to homepages navigation for all users */}
+      <ul className=" pl-5 space-y-2 text-xl py-2 border-t-2 mt-3">
+        <li>
+          <Link to={"/"}>Back To Homepage</Link>
+        </li>
+        <li>
+          <Link to={"/classes"}>Classes</Link>
+        </li>
+        <li>
+          <Link to={"/instructors"}>Instructors</Link>
+        </li>
       </ul>
     </div>
   );
